5105d37a8fde2af577c66a7b1ad5634437b04747
Niklas Gehlen <niklas@namespacelabs.com>
2023-03-14 17:51:56 +0100

    handle action errors gracefully

--- main.ts
+++ main.ts
@@ -17,14 +17,18 @@ Please add a step this step to your workflow's job definition:
 }
 
 async function prepareBuildx(): Promise<void> {
-	const sock = tmpFile("buildkit.sock");
-	await exec.exec(
-		`tmux new-session -d -s ${SessionId} "nsc cluster proxy --kind=buildkit --cluster=build-cluster --sock_path=${sock}"`
-	);
+	try {
+		const sock = tmpFile("buildkit.sock");
+		await exec.exec(
+			`tmux new-session -d -s ${SessionId} "nsc cluster proxy --kind=buildkit --cluster=build-cluster --sock_path=${sock}"`
+		);
 
-	await exec.exec(`docker buildx create --name remote-nsc --driver remote unix://${sock}`);
+		await exec.exec(`docker buildx create --name remote-nsc --driver remote unix://${sock}`);
 
-	await exec.exec("docker buildx use remote-nsc");
+		await exec.exec("docker buildx use remote-nsc");
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
 run();
