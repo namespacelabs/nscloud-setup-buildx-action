c8c2348254dd45a87ceb1aa114f8d30b7760007f
Giulio Micheloni <giulio.micheloni@gmail.com>
2023-03-28 16:51:53 +0200

    Support arm64 builder

--- common.ts
+++ common.ts
@@ -2,6 +2,7 @@ import * as path from "path";
 import * as fs from "fs";
 
 export const ProxyPidFile = tmpFile("buildkit-proxy.pid");
+export const ProxyArmPidFile = tmpFile("buildkit-proxy-arm.pid");
 
 export function tmpFile(file: string): string {
 	const tmpDir = path.join(process.env.RUNNER_TEMP, "ns");
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
-import { ProxyPidFile, tmpFile } from "./common";
+import { ProxyPidFile, ProxyArmPidFile, tmpFile } from "./common";
 
 async function run(): Promise<void> {
 	var commandExists = require("command-exists");
@@ -19,17 +19,27 @@ Please add a step this step to your workflow's job definition:
 async function prepareBuildx(): Promise<void> {
 	try {
 		const sock = tmpFile("buildkit-proxy.sock");
+		const sockArm = tmpFile("buildkit-proxy-arm64.sock");
 
 		await core.group(`Proxy Buildkit from Namespace Cloud`, async () => {
 			// We only need a valid token when opening the proxy
 			await exec.exec("nsc auth exchange-github-token --ensure=5m");
 
-			await exec.exec(
+			const handleAmd = exec.exec(
 				`nsc cluster proxy --kind=buildkit --cluster=build-cluster --sock_path=${sock} --background=${ProxyPidFile}`
 			);
+			const handleArm = exec.exec(
+				`nsc cluster proxy --kind=buildkit --cluster=build-cluster-arm64 --sock_path=${sockArm} --background=${ProxyArmPidFile}`
+			);
+			await handleAmd
+			await handleArm
+			
 
 			await exec.exec(
 				`docker buildx create --name remote-nsc --driver remote unix://${sock} --use`
+			);
+			await exec.exec(
+				`docker buildx create --name remote-nsc --driver remote unix://${sockArm} --append`
 			);
 		});
 
