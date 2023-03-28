3ff431372e86d61d76661f41a0fe1eb66eb567b9
Giulio Micheloni <giulio@namespacelabs.com>
2023-03-28 21:33:07 +0200

    revert prototype of amd build support

--- common.ts
+++ common.ts
@@ -2,7 +2,6 @@ import * as path from "path";
 import * as fs from "fs";
 
 export const ProxyPidFile = tmpFile("buildkit-proxy.pid");
-export const ProxyArmPidFile = tmpFile("buildkit-proxy-arm.pid");
 
 export function tmpFile(file: string): string {
 	const tmpDir = path.join(process.env.RUNNER_TEMP, "ns");
