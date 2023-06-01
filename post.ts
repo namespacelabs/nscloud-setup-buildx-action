17472815753ee72f36aa8b3b37f768ce5c9d167f
Giulio Micheloni <giulio@namespacelabs.com>
2023-06-01 12:20:21 +0200

    post: remove name flag
    Builder name is now stored in state metadata.json file. So, flag --name is no longer needed.

--- post.ts
+++ post.ts
@@ -1,11 +1,10 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
-import { nscRemoteBuilderName } from "./common";
 
 async function run(): Promise<void> {
   try {
     await exec.exec(
-      `nsc docker buildx cleanup --name=${nscRemoteBuilderName}`
+      `nsc docker buildx cleanup`
     );
   } catch (error) {
     core.setFailed(error.message);
