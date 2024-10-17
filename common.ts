6644eff0f9b11fc6bdebe948ba897b5b9ae1f882
Pavol Marko <pavol@namespacelabs.com>
2024-10-17 21:51:10 +0200

    More checks before setting up nsc-remote
    Use `nsc docker buildx status` first to check for presence.
    This does not generate traffic.
    
    Don't set up nsc-remote if in-runner-builder is present.
    The assumption is that a runner profile selected builder is already
    present in this case so this action should not override it.

--- common.ts
+++ common.ts
@@ -1,3 +1,4 @@
 export const nscRemoteBuilderName = "nsc-remote";
+export const nscInRunnerBuilderName = "in-runner-builder"
 export const nscDebugFolder = "/home/runner/nsc";
 export const nscVmIdKey = 'NSC_VM_ID';
